7c2b1870ec76278251df2ebf0d7d2f8c4d7b97f3
liya2023 <122501578+liya2023@users.noreply.github.com>
2023-03-22 22:36:11 -0500

    class 11

---
+++ transitions/transitions2/transitions2.js
@@ -0,0 +1,15 @@
+$(function() {
+    $("#move").click(function() {
+    $("#block1").toggleClass("move");
+    });
+    $("#size").click(function() {
+    $("#block1").toggleClass("size");
+    });
+    $("#opacity").click(function() {
+    $("#block1").toggleClass("opacity");
+    });
+    $("#color").click(function() {
+    $("#block1").toggleClass("color");
+    });
+    });
+    
